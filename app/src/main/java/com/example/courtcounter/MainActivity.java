e75cc03e87e56d6b0a0c9451a403ec6a1b1b2404
Tyson <02tyson.watson@gulllakecs.org>
2020-01-22 10:12:57 -0500

    Added Java code from Gist

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.courtcounter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,12 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
     }
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = (TextView) findViewById(R.id.Points_A);
         scoreView.setText(String.valueOf(score));
     }
 }
