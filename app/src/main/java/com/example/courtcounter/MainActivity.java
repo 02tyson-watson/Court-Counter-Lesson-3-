63922e5933023ec89d1968be34d27fb4124b7a46
Tyson <02tyson.watson@gulllakecs.org>
2020-01-23 09:53:10 -0500

    Added Java code for updating and adding score to both teams

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.courtcounter;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    int totalA = 0;
+    int totalB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+        displayForTeamA(totalA);
+        displayForTeamB(totalB);
     }
     /**
      * Displays the given score for Team A.
@@ -19,5 +23,33 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.Points_A);
         scoreView.setText(String.valueOf(score));
+    }
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.Points_B);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void add_three_A(View view){
+        totalA = totalA + 3;
+        displayForTeamA(totalA);
+    }
+    public void add_two_A(View view){
+        totalA = totalA + 2;
+        displayForTeamA(totalA);
+    }
+    public void add_one_A(View view){
+        totalA = totalA + 1;
+        displayForTeamA(totalA);
+    }
+    public void add_three_B(View view){
+        totalB = totalB + 3;
+        displayForTeamB(totalB);
+    }
+    public void add_two_B(View view){
+        totalB = totalB + 2;
+        displayForTeamB(totalB);
+    }
+    public void add_one_B(View view){
+        totalB = totalB + 1;
+        displayForTeamB(totalB);
     }
 }
